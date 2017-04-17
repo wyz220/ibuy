a4a42b6eb5692f83a52f484a099d327250369e80
wuyanzhong <wuyanzhong@qq.com>
2017-04-17 17:29:05 +0800

    core module

---
+++ ibuy-common-core/src/main/java/com/ibuy/common/ibuy_common_core/App.java
@@ -0,0 +1,13 @@
+package com.ibuy.common.ibuy_common_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
